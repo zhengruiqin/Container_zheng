6beb6a80abc0f639ed8f216072f6edf5a5b01842
zhengruiqin <50261824+zhengruiqin@users.noreply.github.com>
2022-09-06 22:16:56 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+        "fmt"
+        "net/http"
+)
+
+func main() {
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+                fmt.Fprint(w, "welcome to my web")
+        })
+        fs := http.FileServer(http.Dir("static/"))
+        http.Handle("/static/", http.StripPrefix("/static/", fs))
+        http.ListenAndServe(":80", nil)
+}
